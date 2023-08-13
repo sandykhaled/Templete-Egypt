7f2235269971e35a3e66a533acd8ab2ff7422c67
sandy <sandykhaled1203@gmail.com>
2023-08-13 20:16:34 +0300

    project

---
+++ js/style.js
@@ -0,0 +1,12 @@
+
+  btn=document.querySelector(".copy1");
+  btn.onclick=function(){
+    alert('press here');
+   
+  }
+  btn=document.querySelector(".copy2");
+  btn.onclick=function(){
+    alert('press here');
+   
+  }
+
